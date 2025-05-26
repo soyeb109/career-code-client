89e7111b1619f2158829d06e8da4ff402017f7e9
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-26 12:36:10 +0600

    Initial setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl bg-red-600">Job Portal</h2>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;
