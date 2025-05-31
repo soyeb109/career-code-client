df01df54034025ee6e3b6e6ac327d634d0d9935f
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-31 11:20:32 +0600

    show data to job card

--- src/pages/Shared/JobCard.jsx
+++ src/pages/Shared/JobCard.jsx
@@ -1,28 +1,53 @@
 import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { Link } from "react-router";
 
 const JobCard = ({ job }) => {
-  const { title, location, jobType, category, description, company } = job;
+  const {
+    title,
+    location,
+    jobType,
+    category,
+    salaryRange,
+    description,
+    company,
+    requirements,
+    company_logo,
+  } = job;
   return (
     <div>
       <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
+        <div className="flex gap-2 ">
+          <figure>
+            <img className="w-16" src={company_logo} alt="Company Image" />
+          </figure>
+          <div>
+            <h3 className="text-4xl">{company}</h3>
+            <p className="flex items-center gap-1.5">
+              <FaMapMarkerAlt /> {location}
+            </p>
+          </div>
+        </div>
         <div className="card-body">
           <h2 className="card-title">
             {title}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
+            Salary: {salaryRange.min} - {salaryRange.max} {salaryRange.currency}
           </p>
+          <p>{description}</p>
+          <div className="card-actions ">
+            {requirements.map((skill, index) => (
+              <div className="badge badge-outline" key={index}>
+                {skill}
+              </div>
+            ))}
+          </div>
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+            <Link to={`/jobs/${job._id}`}>
+              <button className="btn btn-primary">Apply Now</button>
+            </Link>
           </div>
         </div>
       </div>
