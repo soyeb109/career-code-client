0bcfd0a150fc12506875aa39ce25b585a8ff0650
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-06-02 12:20:22 +0600

    display  my application in table

--- src/pages/MyApplications/ApplicationList.jsx
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { use } from "react";
+import JobApplicationRow from "./JobApplicationRow";
 
-const ApplicationList = () => {
-  return <div>ApplicationList</div>;
+const ApplicationList = ({ myApplicationsPromise }) => {
+  const applications = use(myApplicationsPromise);
+  return (
+    <div>
+      <h3 className="text-3xl">Job Apply so far: {applications.length}</h3>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {applications.map((application, index) => (
+              <JobApplicationRow
+                key={application._id}
+                index={index}
+                application={application}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ApplicationList;
--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import ApplicationStat from "./ApplicationStat";
 import ApplicationList from "./ApplicationList";
 import { Suspense } from "react";
+import useAuth from "../../hooks/useAuth";
+import { myApplicationsPromise } from "../../api/applicationsApi";
 
 const MyApplications = () => {
+  const { user } = useAuth();
+
   return (
     <div>
       <ApplicationStat />
       <Suspense fallback={"loading your applications"}>
-        <ApplicationList />
+        <ApplicationList
+          myApplicationsPromise={myApplicationsPromise(user.email)}
+        />
       </Suspense>
     </div>
   );
